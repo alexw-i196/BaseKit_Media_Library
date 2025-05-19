feaf9d24e7529cdf7d91db185e439a5fd2e5999f
alexw-i196 <awalkeringham@gmail.com>
2025-05-19 13:47:25 +0100

    Updated DefaultState.tsx

--- src/screens/DefaultState/DefaultState.tsx
+++ src/screens/DefaultState/DefaultState.tsx
@@ -22,14 +22,19 @@ export const DefaultState = (): JSX.Element => {
   return (
     <div className="w-full h-screen bg-white flex items-center justify-center p-4 md:p-16">
       <Card className="w-full max-w-[1500px] h-[90vh] rounded-[20px] overflow-hidden shadow-[0px_41px_91px_#0000001a,0px_165px_165px_#00000017,0px_371px_223px_#0000000d,0px_660px_250px_#00000003,0px_1031px_250px_transparent]">
-        <CardContent className="p-0 h-full">
+               <CardContent className="p-0 h-full">
           <div className="flex flex-col w-full h-full">
+            {/* Fixed header */}
             <FrameByAnima onClose={() => setSelectedMedia(null)} />
-            {selectedMedia ? (
-              <MediaDetails media={selectedMedia} onClose={() => setSelectedMedia(null)} />
-            ) : (
-              <FrameWrapperByAnima onMediaSelect={setSelectedMedia} />
-            )}
+            
+            {/* Scrollable content area */}
+            <div className="flex-1 overflow-y-auto">
+              {selectedMedia ? (
+                <MediaDetails media={selectedMedia} onClose={() => setSelectedMedia(null)} />
+              ) : (
+                <FrameWrapperByAnima onMediaSelect={setSelectedMedia} />
+              )}
+            </div>
           </div>
         </CardContent>
       </Card>
